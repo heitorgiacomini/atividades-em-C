c18ebc527a6de27d1f0cf51279815757081feda6
heitor <heithor_thebest@hotmail.com>
2021-09-17 14:45:47 -0300

    commit inicial

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char stringFragmento[20];
+    printf("1) Carregar Arquivo texto. Nessa opção, o sistema deve solicitar para o usuário o nome de"
+    "um arquivo texto e deverá exibir seu conteúdo na tela");
+
+ return 0;
+}
